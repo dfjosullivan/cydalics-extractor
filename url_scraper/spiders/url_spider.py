b8d1a62989daee9d4e0b66a7e09e225305356313
Denis O'Sullivan <denis@cloudaccelerator.co>
2024-03-07 18:30:43 +1000

    Updated script with error handling

--- url_scraper/spiders/url_spider.py
+++ url_scraper/spiders/url_spider.py
@@ -1,13 +1,28 @@
+import logging
+import re
+import shutil
 from pathlib import Path
 
 import scrapy
 import csv
 import os
-from urllib.parse import urljoin
+from urllib.parse import urlparse
 import json
 from bs4 import BeautifulSoup
 class LinkDownloaderSpider(scrapy.Spider):
     name = 'link_downloader'
+    allowed_domains = []
+    logger = logging.getLogger()
+
+    def extract_domains(self, urls):
+        domains = set()
+        for url in urls:
+            try:
+                parsed_url = urlparse(url)
+                domains.add(parsed_url.netloc)
+            except Exception as e:
+                self.logger.exception(e)
+        return domains
 
     def start_requests(self):
         file_folder = Path(__file__).resolve().parents[2]
@@ -18,6 +33,26 @@ class LinkDownloaderSpider(scrapy.Spider):
         else:
             print(f"{file_path} does not exist.")
             raise Exception("File doesn't exist")
+
+        directory_path = Path(__file__).resolve().parents[2].joinpath("downloaded_resources")
+        # Check if the directory exists
+        if os.path.exists(directory_path) and os.path.isdir(directory_path):
+            # Remove the directory and all its contents
+            shutil.rmtree(directory_path)
+            print(f"Directory {directory_path} has been removed.")
+        else:
+            print(f"Directory {directory_path} does not exist.")
+
+        urls = []
+        # Read your CSV file
+        with open(file_path, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Skip the header row
+            for row in reader:
+                url, file_type, depth, comment = row
+                if file_type == "HTML":
+                    urls.append(url)
+        self.allowed_domains = self.extract_domains(urls)
 
         # Read your CSV file
         with open(file_path, 'r') as file:
@@ -95,7 +130,6 @@ class LinkDownloaderSpider(scrapy.Spider):
             'url': response.url,
             'content': extract_content(soup.body)  # Start from the <body> tag
         }
-        print(data)
 
         #yield data
 
@@ -145,7 +179,6 @@ class LinkDownloaderSpider(scrapy.Spider):
                 for element in response.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'], recursive=True):
                     content = {'content': element.get_text(strip=True), 'children': []}
                     if element.name in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-                        print(indent(element.name) + element.text.strip())
                         level = int(element.name[1])
                         current_node = current_hierarchy[level - 1]  # Get the parent node
                         current_node.append(content)  # Append the current content to the parent node
@@ -158,15 +191,124 @@ class LinkDownloaderSpider(scrapy.Spider):
 
                 return nested_dict
             except Exception as e:
+                self.logger.exception(e)
                 return []
 
+        if soup.body is None or soup.body.children is None:
+            self.logger.info("No children found")
+            return {}
+
+
         for child in soup.body.children:
-            print(child.name)
             if child.name in ['main']:
                 content = extract_content(child)
                 return content
 
          # Handle paragraphs without a preceding header
+
+    def url_to_filename(self, url):
+        try:
+            # Remove the protocol (http or https) and replace it with an empty string
+            filename = re.sub(r'^https?://', '', url)
+
+            # Replace invalid filename characters with underscores
+            filename = re.sub(r'[^a-zA-Z0-9\-_\.]', '_', filename)
+
+            # Optional: Trim the length of the filename
+            max_length = 255  # Maximum filename length for most file systems
+            if len(filename) > max_length:
+                extension = filename.rsplit('.', 1)[1]
+                filename = filename[:max_length - len(extension) - 1] + '.' + extension
+
+            return filename
+        except Exception as e:
+            self.logger.exception(e)
+
+    def is_valid_url(self, url):
+        try:
+            result = urlparse(url)
+            return all([result.scheme, result.netloc])
+        except ValueError:
+            return False
+
+    def get_file_folder(self):
+        try:
+            file_folder = Path(__file__).resolve().parents[2].joinpath("downloaded_resources")
+
+            if not file_folder.exists():
+                file_folder.mkdir(parents=True)
+                self.logger.info(f"Folder created: {file_folder}")
+
+            return file_folder
+        except Exception as e:
+            self.logger.exception(e)
+            raise Exception("Failed to get folder")
+
+    def update_downloaded_csv(self, current_url, file_type):
+        try:
+            file_folder = self.get_file_folder()
+            filename = file_folder.joinpath("downloaded.csv")
+            new_row = [current_url, file_type]
+
+            with open(filename, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(new_row)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def save_pdf(self,
+                 response):
+        try:
+            file_folder = self.get_file_folder()
+            filename = response.url.split('/')[-1]
+            filename = file_folder.joinpath(filename)
+            with open(str(filename), 'wb') as f:
+                f.write(response.body)
+            self.logger.info(f'Saved PDF: {filename}')
+        except Exception as e:
+            self.logger.exception(e)
+
+    def update_url_skipped(self,
+                           current_url,
+                           context_url,
+                           file_type,
+                           reason):
+        try:
+            file_folder = self.get_file_folder()
+            filename = file_folder.joinpath("downloaded.csv")
+            new_row = [current_url, context_url, file_type, reason]
+
+            with open(filename, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(new_row)
+        except Exception as e:
+            self.logger.exception(e)
+
+    def save_html(self,
+                  response,
+                  current_url,
+                  file_type):
+        try:
+            if response is None or response.text is None:
+                self.update_url_skipped(current_url, file_type, current_url, "Response text is empty ")
+                self.logger.info("Skipping " + current_url + " not valid ")
+                return
+
+            content = {}
+            content["content"] = self.parse_html2(response)
+
+            content["file_type"] = file_type
+            content["url"] = current_url
+            # Save the PDF file
+            file_folder = self.get_file_folder()
+
+            filename = file_folder.joinpath(self.url_to_filename(current_url))
+            # Writing JSON data
+            with open(str(filename) + ".json", 'w') as f:
+                json.dump(content, f)
+            self.logger.info("Downloaded page " + str(filename))
+        except Exception as e:
+            self.logger.exception(e)
 
     def parse(self, response):
 
@@ -175,23 +317,41 @@ class LinkDownloaderSpider(scrapy.Spider):
         depth = response.meta['depth']
         current_url = response.url
 
-        print(current_url)
+        self.logger.info("Processing " + current_url)
+
+        self.update_downloaded_csv(current_url, file_type)
 
         if file_type == 'PDF':
             # Save the PDF file
-            file_folder = Path(__file__).resolve().parents[2].joinpath("downloaded_resources")
-            if not file_folder.exists():
-                file_folder.mkdir(parents=True)
-                print(f"Folder created: {file_folder}")
+            self.save_pdf(response)
 
-            filename = response.url.split('/')[-1]
-            with open(filename, 'wb') as f:
-                f.write(response.body)
-            self.log(f'Saved PDF: {filename}')
+        elif file_type == 'HTML':
+            self.save_html(response, current_url, file_type)
 
-        elif file_type == 'HTML' and depth > 0:
-            #data = self.parse_html(response)
-            #print(1)
-            self.parse_html2(response)
+            # Follow links within the same domain only, respecting the depth limit
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for a in soup.find_all('a', href=True):
+                link = a['href']
+                # Check if the link is within the same domain
+                if not self.is_valid_url(link):
+                    self.update_url_skipped(link, file_type, current_url, "Response text is empty ")
+                    self.logger.info("Skipping " + link + " not valid ")
+                    continue
+
+                if link.lower().endswith('.pdf') or "pdf" in link.lower():
+                    new_file_type = "PDF"
+                else:
+                    new_file_type = "HTML"
+
+                if urlparse(link).netloc == '' or urlparse(link).netloc in self.allowed_domains:
+                    self.logger.info("Added " + link)
+                    yield response.follow(link, self.parse, meta={'file_type': new_file_type, 'depth': 2, 'original_url': link})
+                elif urlparse(link).netloc not in self.allowed_domains:
+                    self.logger.info("Did not add (domain) " + link)
+                    self.update_url_skipped(link, file_type, current_url, "Outside allowed domains")
+                elif urlparse(link).netloc:
+                    self.logger.info("Did not add (url) " + link)
+                    self.update_url_skipped(link, file_type, current_url, "Not url")
+
 
 
